ed0788ff4ef1fef0a938b6c22b2380467de135ae
Ben Wesson <Ben.Wesson@3squared.com>
2019-07-22 17:11:27 +0100

    Start barebones of website

---
+++ personal-site/src/header/Face.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="image-background">
+    <div class="image-cropper">
+        <img alt="Face" src="..\assets\Face.png">
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+        name: 'Face',
+    }
+</script>
+
+<style lang="scss" scoped>
+img {
+    display: inline;
+    margin: 0 auto;
+    height: 100%;
+    width: auto;
+}
+.image-cropper {
+    width: 120px;
+    height: 120px;
+    position: relative;
+    overflow: hidden;
+    border-radius: 50%;
+    margin: 5px 5px;
+}
+
+.image-background {
+    width: 150px;
+    height: 150px;
+    position: relative;
+    overflow: hidden;
+    border-radius: 0% 0% 102px 0px;
+    background-color: #243447;
+}
+</style>
---
+++ personal-site/src/header/Header.vue
@@ -0,0 +1,52 @@
+<template>
+<div>
+    <Face class="face"/>
+    <nav>
+        <ul>
+            <li><a href="Developer.html">Developer</a></li>
+            <li><a href="Scientist.html">Scientist</a></li>
+            <li><a href="Dancer.html">Dancer</a></li>
+        </ul>
+    </nav>
+</div>
+</template>
+
+<script>
+import Face from './Face.vue';
+export default {
+    name: 'Header',
+    components : {
+        Face,
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../shared/colour-palette.scss";
+.face {
+    float:left;
+    background-color: $highlight-colour;
+}
+nav {
+    background-color: $highlight-colour;
+    margin: 0;
+    overflow: hidden;
+}
+nav ul{
+    float:left;
+    margin: 0;
+    padding: 0;
+}
+nav ul li {
+    display: inline-block;
+    list-style-type: none;
+}
+nav > ul > li > a {
+    color: #ffffff;
+    background-color: $highlight-colour;
+    display: block;
+    line-height: 2em;
+    padding: 0.5em 0.5em;
+    text-decoration: none;
+}
+</style>
