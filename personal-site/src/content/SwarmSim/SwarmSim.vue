35c19a1c2579c1d147fb833c478364118519e3a6
Ben Wesson <Ben.Wesson@3squared.com>
2019-07-23 17:34:42 +0100

    Add measurement of success for the framerate

--- personal-site/src/content/SwarmSim/SwarmSim.vue
+++ personal-site/src/content/SwarmSim/SwarmSim.vue
@@ -9,6 +9,7 @@
             <div>Intended time per frame {{intendedRate}} ms</div>
             <div>Average Time Taken for make request over last 5 frames {{getAverageCallTime}} ms</div>
             <div>Time waiting before next call {{getTimeToWaitBeforeNextFrame}} ms</div>
+            <div>Achieved average round trip time of {{getAverageRoundTripTime}} ms over last 10 calls </div>
         </div>
         <button v-on:click="startSim()" class="next-frame-button"> Start </button>
         <button v-on:click="stopSim()" class="next-frame-button"> Stop </button>
@@ -31,7 +32,8 @@ export default {
         return {
             runSimFlag : false,
             intendedRate : 1500,
-            lastFiveCalls : []
+            lastFiveCalls : [],
+            lastTenRoundTripTimes : []
         }
     },
     computed: {
@@ -39,20 +41,24 @@ export default {
             return this.$store.state.SwarmSimFrame;
         },
         getAverageCallTime() {
-            return this.lastFiveCalls.reduce(function(p,c,i){return p+(c-p)/(i+1)},0);
+            return Math.round(this.lastFiveCalls.reduce(function(p,c,i){return p+(c-p)/(i+1)},0));
         },
         getTimeToWaitBeforeNextFrame() {
-            var timeToWait = Math.max(this.intendedRate-this.getAverageCallTime,0);
-            console.log(timeToWait)
-            return timeToWait;
+            return Math.round(Math.max(this.intendedRate-this.getAverageCallTime,0));
+        },
+        getAverageRoundTripTime() {
+            return Math.round(this.lastTenRoundTripTimes.reduce(function(p,c,i){return p+(c-p)/(i+1)},0));
         },
     },
     methods:{
         async startSim() {
             this.runSimFlag = true;
             do{
+                var startTime = new Date();
                 var promise = new Promise((resolve) => setTimeout(resolve, this.getTimeToWaitBeforeNextFrame));
                 await promise.then(this.getNextFrame);
+                var endTime = new Date();
+                this.AddToRoundTripBuffer(endTime - startTime);
             } while (this.runSimFlag)         
         },
         stopSim() {
@@ -62,16 +68,22 @@ export default {
             var startTime = new Date();
             await this.$store.dispatch('nextFrame');
             var endTime = new Date();
-            this.AddToBuffer(endTime - startTime);
+            this.AddToTimesTakenBuffer(endTime - startTime);
         },
         resetFrame() {
             this.$store.dispatch('initFrame');
             this.stopGame();
         },
-        AddToBuffer (timeTaken) {
+        AddToTimesTakenBuffer (timeTaken) {
             this.lastFiveCalls.push(timeTaken);
             if (this.lastFiveCalls.length > 5) {
                 this.lastFiveCalls.shift();
+            }
+        },
+        AddToRoundTripBuffer (timeTaken) {
+            this.lastTenRoundTripTimes.push(timeTaken);
+            if (this.lastTenRoundTripTimes.length > 10) {
+                this.lastTenRoundTripTimes.shift();
             }
         },
     }
