f32d7aee5ba717166f983452f8b0d64bdcbcaa9c
Ben Wesson <Ben.Wesson@3squared.com>
2019-07-23 11:15:03 +0100

    Format the maze into something human readable

--- personal-site/src/content/SwarmSim/SwarmSimSpace.vue
+++ personal-site/src/content/SwarmSim/SwarmSimSpace.vue
@@ -1,6 +1,11 @@
 <template>
-    <div class="space">
-        {{this.state}}
+    <div class="space-wrapper">
+        <div v-if="this.state == 0" class="space wall"></div>
+        <div v-if="this.state == 1" class="space unexplored"></div>
+        <div v-if="this.state == 2" class="space explored"></div>
+        <div v-if="this.state == 3" class="space ungrouped-drone"></div>
+        <div v-if="this.state == 4" class="space leader-drone"></div>
+        <div v-if="this.state == 5" class="space subordinate-drone"></div>
     </div>
 </template>
 
@@ -18,7 +23,30 @@
 </script>
 
 <style lang="scss" scoped>
-.space {
+@import "./swarmsim-palette.scss";
+.space-wrapper {
     float: left;
+}
+.space {
+    height:18px;
+    width:18px;
+}
+.wall{
+    background-color: $wall;
+}
+.unexplored{
+    background-color: $unexplored;
+}
+.explored{
+    background-color:$explored;
+}
+.ungrouped-drone{
+    background-color:$ungrouped-drone;
+}
+.leader-drone{
+    background-color:$leader-drone;
+}
+.subordinate-drone{
+    background-color:$subordinate-drone;
 }
 </style>
