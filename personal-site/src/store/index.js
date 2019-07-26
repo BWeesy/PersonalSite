2a7fcc203dc1e69f9957895a2008d326a884d048
Ben Wesson <Ben.Wesson@3squared.com>
2019-07-26 16:42:16 +0100

    Reset the frame rate on reset of sim

--- personal-site/src/store/index.js
+++ personal-site/src/store/index.js
@@ -28,6 +28,7 @@ export default new Vuex.Store({
     actions: {
         initFrame({commit}){
             commit('saveNewFrame', []);
+            commit('saveSwarmSimFrameRate', 1000);
 
             var url = process.env.NODE_ENV == 'development' ? '/api/InitFrame' : 'https://swarmsim.azurewebsites.net/api/InitFrame?code=GiWQXWY1cb/nO4Mw9TOVuElPZGLidWSV0ulanCq6TRTDyKxGwXMI0w==';
             axios.get(url, { crossdomain: true })
